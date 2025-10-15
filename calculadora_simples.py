6a1f38415661ba8da179cf8100a00cddea3bb02d
ari-domingos <ariellebeatriz1005@gmail.com>
2025-10-14 21:18:18 -0300

    atualização

--- calculadora_simples.py
+++ calculadora_simples.py
@@ -2,60 +2,70 @@
 import os
 sistema = True
 
-print('------------------------------------')
-print('   CALCULADORA SIMPLES (2 VALORES)')
-print('         NÚMEROS INTEIROS')
-print('------------------------------------')
-print('1 - SOMA')
-print('2 - SUBTRAÇÃO')
-print('3 - MULTIPICAÇÃO')
-print('4 - DIVISÃO')
-print('------------------------------------')
-operacao = int(input('Escolha a operação que deseja: '))
-
 while sistema == True:
+    os.system('clear')
+    print('====================================')
+    print('   CALCULADORA SIMPLES (2 VALORES)  ')
+    print('====================================')
+    print('1 - Soma')
+    print('2 - Subtração')
+    print('3 - Multiplicação')
+    print('4 - Divisão')
+    print('\n0 - Sair')
+    print('====================================')
+    operacao = float(input('Escolha a operação que deseja: '))
+
     if operacao == 1:
         os.system('clear')
-        
-        print('------------------------')
+        print('========================')
         print('    CALCULADORA SOMA    ')
-        print('------------------------')
-        valorSomaUm = int(input('Digite o primeiro valor: '))
-        valorSomaDois = int(input('Digite o segundo valor: '))
+        print('========================')
+        valorSomaUm = float(input('Digite o primeiro valor: '))
+        valorSomaDois = float(input('Digite o segundo valor: '))
         calculoSoma = valorSomaUm + valorSomaDois
-        print(f'A soma dos números {valorSomaUm} e {valorSomaDois} é: {calculoSoma}')
-        break
-
+        print(f'A soma dos números {valorSomaUm:.2f} e {valorSomaDois:.2f} é: {calculoSoma:.2f}')
+        print('--------------------------------------')
+        input("Pressione ENTER para voltar ao menu...")
     elif operacao == 2:
         os.system('clear')
-        
-        print('-----------------------------')
+        print('=============================')
         print('    CALCULADORA SUBTRAÇÃO    ')
-        print('-----------------------------')
-        valorSubtracaoUm = int(input('Digite o primeiro valor: '))
-        valorSubtracaoDois = int(input('Digite o segundo valor: '))
+        print('=============================')
+        valorSubtracaoUm = float(input('Digite o primeiro valor: '))
+        valorSubtracaoDois = float(input('Digite o segundo valor: '))
         calculoSubtracao = valorSubtracaoUm - valorSubtracaoDois
-        print(f'A subtração dos números {valorSubtracaoUm} e {valorDivisaoDois} é: {calculoSubtracao}')
-        break
+        print(f'A subtração dos números {valorSubtracaoUm:.2f} e {valorSubtracaoDois:.2f} é: {calculoSubtracao:.2f}')
+        print('--------------------------------------')
+        input("Pressione ENTER para voltar ao menu...")
     elif operacao == 3:
-        os.system('clear')
-        
-        print('--------------------------------')
+        os.system('clear')        
+        print('================================')
         print('    CALCULADORA MULTIPICAÇÃO    ')
-        print('--------------------------------')
-        valorMultipicacaoUm = int(input('Digite o primeiro valor: '))
-        valorMultipicacaoDois = int(input('Digite o segundo valor: '))
+        print('================================')
+        valorMultipicacaoUm = float(input('Digite o primeiro valor: '))
+        valorMultipicacaoDois = float(input('Digite o segundo valor: '))
         calculoMultiplicacao = valorMultipicacaoUm * valorMultipicacaoDois
-        print(f'A multiplicação dos números {valorMultipicacaoUm} e {valorMultipicacaoDois} é: {calculoMultiplicacao}')
-        break
-    else:
+        print(f'A multiplicação dos números {valorMultipicacaoUm:.2f} e {valorMultipicacaoDois:.2f} é: {calculoMultiplicacao:.2f}')
+        print('--------------------------------------')
+        input("Pressione ENTER para voltar ao menu...")
+    elif operacao == 4:
         os.system('clear')
-        
-        print('---------------------------')
+        print('===========================')
         print('    CALCULADORA DIVISÃO    ')
-        print('---------------------------')
-        valorDivisaoUm = int(input('Digite o primeiro valor: '))
-        valorDivisaoDois = int(input('Digite o segundo valor: '))
-        calculoDivisao = valorDivisaoUm // valorDivisaoDois
-        print(f'A divisão dos números {valorDivisaoUm} e {valorDivisaoDois} é: {calculoDivisao}')
-        break
+        print('===========================')
+        valorDivisaoUm = float(input('Digite o primeiro valor: '))
+        valorDivisaoDois = float(input('Digite o segundo valor: '))
+        calculoDivisao = valorDivisaoUm / valorDivisaoDois
+        print(f'A divisão dos números {valorDivisaoUm:.2f} e {valorDivisaoDois:.2f} é: {calculoDivisao:.2f}')
+        print('--------------------------------------')
+        input("Pressione ENTER para voltar ao menu...")
+    elif operacao == 0:
+        os.system('clear')
+        print('===============')
+        print('    SAINDO...  ')
+        print('===============')
+        sistema = False
+    else:
+        print('===================')
+        print('  OPÇÃO INVÁLIDA!  ')
+        print('===================')
